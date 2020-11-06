c574f242adea1da3799e74c69633df3cabb40db7
SangBatosai-Code5 <65680662+sangbatosai-code5@users.noreply.github.com>
2020-11-06 09:30:15 -0500

    Update like_htag.py

--- modulenya/modul_like/like_htag.py
+++ modulenya/modul_like/like_htag.py
@@ -72,7 +72,7 @@ class LTAG:
 		print ()
 	    
 	def loginakun(self):
-		self.driver_path = "../WebDriver/geckodriver"
+		self.driver_path = "/usr/bin/geckodriver"
 		self.mybrowser = webdriver.Firefox(executable_path=self.driver_path)
 		mybrowser=self.mybrowser
 		mybrowser.implicitly_wait(10)
